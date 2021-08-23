7d32960accb02eb8a0eb1d5245bbc6f89344ef76
Hollister009 <reactive.andrew@yahoo.com.ua>
2021-08-23 23:28:20 +0300

    TailwindCSS integration

--- src/components/TestComponent.tsx
+++ src/components/TestComponent.tsx
@@ -3,7 +3,7 @@ import React from 'react';
 export interface TestComponentProps {}
 
 const TestComponent = () => {
-  return <div>TestComponent</div>;
+  return <div className="bg-white p-6 border rounded-sm">TestComponent</div>;
 };
 
 export default TestComponent;
